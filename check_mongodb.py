Multiposting/nagios-plugin-mongodb

commit 0c83e66e3d81f12ad5b135fbaf50615717d7d49f

Fixed replication_lag failure when not running with --replSet

check_mongodb.py
@@ -294,7 +294,13 @@ def check_rep_lag(con,  warning, critical, perf_data,max_lag):
         set_read_preference(con.admin)
 
         # Get replica set status
-        rs_status = con.admin.command("replSetGetStatus")
+        try:
+            rs_status = con.admin.command("replSetGetStatus")
+        except pymongo.errors.OperationFailure,e :
+            if e.code==None and str(e).find('failed: not running with --replSet"'):
+                print "OK - Not running with replSet"
+                return 0
+        
         rs_conf = con.local.system.replset.find_one()
         for member in rs_conf['members']:
             if member.get('slaveDelay') is not None:

commit 621f21cd61729768d9d5de8c3f6fd32fed92be5b

Raise a critical if we can't read the replication status.
Exception handling introduced by 0c83e66e was completely broken.
find() was incorrectly used and matched all received error messages
reporting that was OK.
Also, there is no reason to respond OK, if we check a replication lag
and the node does not run with --replSet.

check_mongodb.py
@@ -348,9 +348,8 @@ def check_rep_lag(con, host, port, warning, critical, percent, perf_data, max_la
         try:
             rs_status = con.admin.command("replSetGetStatus")
         except pymongo.errors.OperationFailure, e:
-            if e.code == None and str(e).find('failed: not running with --replSet"'):
-                print "OK - Not running with replSet"
-                return 0
+            print "CRITICAL - %s" % str(e)
+            return 2
 
         serverVersion = tuple(con.server_info()['version'].split('.'))
         if serverVersion >= tuple("2.0.0".split(".")):